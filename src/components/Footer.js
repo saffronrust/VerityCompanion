5dfa14aa7df6911ce87d05664477f349a9c1bce8
Zheng Jie <zhengjie@istaging.com>
2024-07-16 13:44:08 +0800

    edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,30 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import MuiLink from '@mui/material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Footer() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
+    <>
+    <Typography variant="body2" color="text.secondary" align="center" sx={{ my: 1 }}>
       {'Made by '}
       <MuiLink color="inherit" href="https://github.com/saffronrust/VerityCompanion">
         saffronrust
       </MuiLink>
+      {' '}
+      <GitHubIcon />
     </Typography>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Powered by '}
+      <MuiLink color="inherit" href="https://mui.com/">
+        Material-UI
+      </MuiLink>
+      {' and '}
+      <MuiLink color="inherit" href="https://nextjs.org/">
+        Next.js
+      </MuiLink>
+      {'.'}
+    </Typography>
+    </>
   );
 }
